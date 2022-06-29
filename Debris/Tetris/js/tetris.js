4b3bccf74effe60ea9042809d198698481c3a764
Yooodh <eogus2604@hanmail.net>
2022-06-29 20:00:25 +0900

    v0.3

--- Debris/Tetris/js/tetris.js
+++ Debris/Tetris/js/tetris.js
@@ -31,7 +31,7 @@ const movingItem = {
   type: 'tree',
   direction: 0, // 화살표 위 방향키를 눌렀을 때 돌아가게 하는 역할을 하는 지표
   top: 0, // 좌표 기준 어디까지 내려와있는지, 내려가야 하는지 표현해주는 역할
-  left: 0, // 좌우 값을 알려주는 역할
+  left: 3, // 좌우 값을 알려주는 역할
 };
 
 // 처음에 랜더링이 되면 init 호출
@@ -72,17 +72,100 @@ function renderBlocks() {
   // tempMovingItem.top;
   // tempMovingItem.left;
 
+  const movingBlocks = document.querySelectorAll('.moving');
+  movingBlocks.forEach((moving) => {
+    // 타입(tree)과 moving을 빼준다.
+    moving.classList.remove(type, 'moving');
+  });
+
   // forEach() : 배열에 활용 가능한 메서드로, 파라미터로 주어진 함수를 배역 요소 각각에 대해 실행하는 메서드이다.
   // map() 메서드와 차이점은 따로 return 하는 값이 없다는 점이다.
   BLOCKS[type][direction].forEach((block) => {
     // ul안에 들어있는 li의 값
-    const x = block[0];
+    const x = block[0] + left;
 
     // li의 low값
-    const y = block[1];
+    const y = block[1] + top;
 
-    const target = playground.childNodes[y].childNodes[0].childNodes[x];
-    target.classList.add(type);
-    console.log(target);
+    // 3항 연산자
+    // 조건 ? 참일경우 : 거짓일경우
+    // const xxx = 조건 ? 참일경우 : 거짓일경우
+    const target = playground.childNodes[y]
+      ? playground.childNodes[y].childNodes[0].childNodes[x]
+      : null;
+
+    const isAvailable = checkEmpty(target);
+
+    if (isAvailable) {
+      target.classList.add(type, 'moving');
+    } else {
+      // tempMovingItem값을 원래대로 돌리기
+      tempMovingItem = { ...movingItem };
+
+      // renderBlocks()를 다시 한번 실행 시키는 재귀함수 호출
+      // 콜스택 에러를 방지하기 위해 이벤트 루프 안에 넣지 말고 외부로 빼놓았다가
+      // 태스크 큐에 넣어놓았다가 이벤트 루프가 모두 실행 된 후에
+      // 다시 실행할 수 있도록 setTimeout으로 잠시 빼놓는다.
+      setTimeout(() => {
+        renderBlocks();
+
+        // 밑으로 떨어지는 중에 없는 것이 발생하게되면
+        if (moveType === 'top') {
+          // 블럭을 고정시키는 함수 호출
+          seizeBlock();
+        }
+      }, 0);
+    }
   });
+  // render가 성공할 때마다 값 변경
+  // 한 바퀴를 돌고나서 정상적으로 작동되면 movingItem을 업데이트 시켜준다.
+  // 돌다가 실행이 되지 않으면 tempMovingItem을 movingItem으로 다시 집어넣고
+  // 다시 한번 renderBlocks()를 호출 해서 다시 한번 모양이 나오게 된다.
+  movingItem.left = left;
+  movingItem.top = top;
+  movingItem.direction = direction;
 }
+
+// 끝에서 더이상 내려갈 곳이 없으면 색깔은 그대로 놔두고
+// moving이라는 클래스를 다 뗀 후에
+// 새로운 블럭 만들기
+function seizeBlock() {
+  console.log('seize block');
+}
+
+// target을 한 번 더 체크하는 함수 생성
+// 빈 여백을 체크해서 밖으로 벗어나지 않도록 체크
+// 블럭이 맨 하단으로 떨어졌을 때 또 다른 블럭이 생성되고
+// 그 블럭이 블럭위에 떨어졌을 때 그 밑에 블럭이 있는지 없는지 체크
+function checkEmpty(target) {
+  if (!target) {
+    return false;
+  }
+  return true;
+}
+
+function moveBlock(moveType, amount) {
+  // movingItem 안에 들어있는 left를 변경
+  tempMovingItem[moveType] += amount;
+  renderBlocks();
+}
+// renderBlocks();
+
+/* event handling */
+document.addEventListener('keydown', (e) => {
+  switch (e.keyCode) {
+    case 39:
+      moveBlock('left', 1);
+      break;
+
+    case 37:
+      moveBlock('left', -1);
+      break;
+
+    case 40:
+      moveBlock('top', 1);
+      break;
+    default:
+      break;
+  }
+});
