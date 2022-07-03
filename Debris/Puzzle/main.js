bf5b61af16c98a0fc56c2f615ac9979821964745
Yooodh <eogus2604@hanmail.net>
2022-07-03 17:56:31 +0900

    v0.4

--- Debris/Puzzle/main.js
+++ Debris/Puzzle/main.js
@@ -25,3 +25,20 @@ Array(tileCount)
     // DOM에 넣어주기
     container.appendChild(li);
   });
+
+  // 순서 섞기
+  function shuffle(array) { // 배열 넘겨받기
+    // 배열의 index를 구해서 -1 해준다. (가장 마지막 인덱스 선택)
+    let index = array.length -1;
+    // 가장 마지막 index가 1씩 감소하면서 0보다 크면 반복 (뒤에서부터 앞으로 진행)
+    while(index > 0) {
+      // Math.floor() : 소수점 이하를 버림
+      // Math.cell() : 소수점 이하를 올림
+      // Math.round() : 소수점 이하를 반올림
+      // Math.random() : 난수(무작위 숫자) 생성
+      const randomIndex = Math.floor( Math.random() * (index+1) );
+      [array[index], array[randomIndex]] = [array[randomIndex], array[index]]
+      index --;
+    }
+    return array;
+  }
