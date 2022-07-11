b5b5d4e23460852d503bb9f2f9b06b693e57c646
Yooodh <eogus2604@hanmail.net>
2022-07-11 19:08:40 +0900

    v0.4

--- Debris/Chat/app.js
+++ Debris/Chat/app.js
@@ -13,9 +13,24 @@ app.use(express.static(path.join(__dirname, 'src')));
 const PORT = process.env.PORT || 5000; // process 환경에 PORT가 지정이 됐으면 그것을 사용하고 아니라면 5000번 사용
 
 io.on('connection', (socket) => {
-  console.log('연결이 이루어 졌습니다');
+  // console.log('연결이 이루어 졌습니다.');
+
+  // 서버에서 받아주기
+  // socket.on('채팅아이디', 실행할 함수)
+  socket.on('chatting', (data) => {
+    console.log(data);
+
+    // 메세지 되돌려주기
+    io.emit('chatting', `그래 반가워 ${data}`);
+  });
 });
 
 // 서버 실행 명령어
 // listen(포트, 실행할 명령)
 server.listen(PORT, () => console.log(`sever is running ${PORT}`));
+
+/**
+ * 1. emit을 통해 보내고
+ * 2. 서버에서 다시 emit을 통해서 fornt end쪽으로 보내고
+ * 3. front end가 메시지를 받은 것을 확인할 수 있다.
+ */
--- Debris/Chat/src/js/chat.js
+++ Debris/Chat/src/js/chat.js
@@ -1,1 +1,23 @@
-console.log('hello js');
+'use strict';
+
+// socket에 클라이언트 socket io 담기
+const socket = io();
+
+// socket 객체를 이용해서 메시지 보내기
+// socket.emit(채널아이디, 내용);
+socket.emit('chatting', 'from front');
+
+// 받아주기
+// socket.on('채팅아이디', 실행할 함수)
+socket.on('chatting', (data) => {
+  console.log(data);
+});
+
+// 정상적으로 불러와졌는지 확인
+console.log(socket);
+
+/**
+ * 1. emit을 통해 보내고
+ * 2. 서버에서 다시 emit을 통해서 fornt end쪽으로 보내고
+ * 3. front end가 메시지를 받은 것을 확인할 수 있다.
+ */
