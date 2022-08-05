435980ea3dcc0bd53df2ae5c57b6782b4397dd27
Yooodh <eogus2604@hanmail.net>
2022-08-05 16:24:09 +0900

    v0.5

--- Debris/TypingGame/js/main.js
+++ Debris/TypingGame/js/main.js
@@ -1,7 +1,12 @@
 // 사용변수
+const GAME_TIME = 3;
+
 let score = 0;
-let time = 9;
+let time = GAME_TIME;
 let isPlaying = false;
+let timeInterval;
+let checkInterval;
+let words = [];
 
 const wordInput = document.querySelector('.word-input');
 // console.log(wordInput);
@@ -12,25 +17,73 @@ const scoreDisplay = document.querySelector('.score');
 
 const timeDisplay = document.querySelector('.time');
 
-// wordInput.addEventListener('이벤트','기능')
-wordInput.addEventListener('input', () => {
+const button = document.querySelector('.button');
+
+init();
+
+function init() {
+  getWords();
+
+  // wordInput.addEventListener('이벤트','기능')
+  wordInput.addEventListener('Input', checkMatch);
+}
+
+// 게임 실행
+function run() {
+  isPlaying = true;
+  time = GAME_TIME;
+  wordInput.focus();
+  scoreDisplay.innerText = 0;
+  timeInterval = setInterval(countDown, 1000);
+  checkInterval = setInterval(checkStatus, 50);
+  buttonChange('게임중');
+}
+
+function checkStatus() {
+  if (!isPlaying && time === 0) {
+    buttonChange('게임시작');
+    clearInterval(checkInterval);
+  }
+}
+
+// 단어 불러오기
+function getWords() {
+  words = ['Hello', 'Banana', 'Apple', 'Cherry'];
+  buttonChange('게임시작');
+}
+
+// 단어 일치 체크
+function checkMatch() {
   // console.log(
   //   wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()
   // );
   if (wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
-    score++;
-    scoreDisplay.innerText = score;
-
     // input 창 초기화 시켜주기
     wordInput.value = '';
+    if (!isPlaying) {
+      return;
+    }
+    score++;
+    scoreDisplay.innerText = score;
+    time = GAME_TIME;
+    const randomIndex = Math.floor(Math.random() * words.length);
+    wordDisplay.innerText = words[randomIndex];
   }
-});
-
-setInterval(countDown, 1000);
+}
 
 function countDown() {
   // 3항 연산자
   // (조건) ? 참일결우 : 거짓일경우
   time > 0 ? time-- : (isPlaying = false);
+  if (!isPlaying) {
+    clearInterval(timeInterval);
+  }
   timeDisplay.innerText = time;
+}
+
+function buttonChange(text) {
+  button.innerText = text;
+  text === '게임시작'
+    ? button.classList.remove('loading')
+    : button.classList.add('loading');
 }
