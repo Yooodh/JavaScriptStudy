f9dd64560809a464be7ab1dc848876f8aeccc0bc
Yooodh <eogus2604@hanmail.net>
2022-08-05 15:45:19 +0900

    v0.4

--- Debris/TypingGame/js/main.js
+++ Debris/TypingGame/js/main.js
@@ -1,5 +1,7 @@
 // 사용변수
 let score = 0;
+let time = 9;
+let isPlaying = false;
 
 const wordInput = document.querySelector('.word-input');
 // console.log(wordInput);
@@ -7,6 +9,8 @@ const wordInput = document.querySelector('.word-input');
 const wordDisplay = document.querySelector('.word-display');
 
 const scoreDisplay = document.querySelector('.score');
+
+const timeDisplay = document.querySelector('.time');
 
 // wordInput.addEventListener('이벤트','기능')
 wordInput.addEventListener('input', () => {
@@ -21,3 +25,12 @@ wordInput.addEventListener('input', () => {
     wordInput.value = '';
   }
 });
+
+setInterval(countDown, 1000);
+
+function countDown() {
+  // 3항 연산자
+  // (조건) ? 참일결우 : 거짓일경우
+  time > 0 ? time-- : (isPlaying = false);
+  timeDisplay.innerText = time;
+}
